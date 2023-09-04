9bd90a8d73eb85f3bc02abd71955611e8ca0a162
Gourav Soni <84095227+gouravg8@users.noreply.github.com>
2023-09-04 20:57:46 +0530

    Delete JS directory

--- JS/GUiProjects/worldCountriesList.js
+++
@@ -1,9 +0,0 @@
-import data from "./data.mjs";
-
-let countryListContainer = document.querySelector(".countriesList");
-data.forEach((countryName) => {
-  const countryElement = document.createElement("p");
-  countryElement.textContent = countryName;
-  countryListContainer.appendChild(countryElement);
-});
-// console.log(data);
--- JS/GUiProjects/world_countries_data_visualization_1/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = ["./dist/**/*.{html,js}"];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
